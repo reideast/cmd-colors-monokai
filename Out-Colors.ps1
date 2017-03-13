95884c00b3c5f84f0368bc8cf92d29df38f6e494
Andrew Reid East <andrew@andreweast.net>
2017-03-13 00:50:04 +0000

    Simplify demo script to simply display colors

--- Out-Colors.ps1
+++ Out-Colors.ps1
@@ -1,8 +1,5 @@
-function Out-Colors()
-{
-    [enum]::GetValues([ConsoleColor]) | % {
-        Write-Host ('{0,2:N0} ' -f $_.value__) -NoNewline
-        Write-Host ('{0,-12}' -f $_) -ForegroundColor $_ -NoNewLine
-        Write-Host "`t" -BackgroundColor $_
-    }
+[enum]::GetValues([ConsoleColor]) | % {
+    Write-Host ('{0,2:N0} ' -f $_.value__) -NoNewline
+    Write-Host ('{0,-12}' -f $_) -ForegroundColor $_ -NoNewLine
+    Write-Host "`t" -BackgroundColor $_
 }
